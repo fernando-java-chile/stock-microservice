e767606c31ee3d22a5d9e54ee5e2a02cd9df87b2
fernando-java-chile <fernando.java.chile@gmail.com>
2025-03-19 15:02:35 -0300

    first commit

---
+++ src/test/java/com/StockProduct/stock_microservice/StockMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StockProduct.stock_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
